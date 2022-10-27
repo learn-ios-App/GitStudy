114df70b123c5badc5b7213d250201a679c4cfd6
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2022-10-27 12:21:23 +0900

    Initial Commit

---
+++ GitStudy/GitStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitStudyApp.swift
+//  GitStudy
+//
+//  Created by 渡邊魁優 on 2022/10/27.
+//
+
+import SwiftUI
+
+@main
+struct GitStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
