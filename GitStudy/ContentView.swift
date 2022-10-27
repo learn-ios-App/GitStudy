90881ee3f2e9d19d02be2557d13a7c2418fe0e56
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2022-10-27 12:27:12 +0900

    commit

--- GitStudy/ContentView.swift
+++ GitStudy/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, swift")
+            Text("Hello, swift!!")
         }
         .padding()
     }
